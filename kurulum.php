d325f6ba7ce3c689e9d1eb97b9f33f06d1c89c8f
Ahmed Goş <gosahmet@gmail.com>
2014-12-03 16:52:05 +0200

    Değiştirme

--- kurulum.php
+++ kurulum.php
@@ -1,5 +1,6 @@
 ﻿<html>
 <body>
+<h1>Yeni Bilgi.</h1>
 <iframe width="560" height="315" src="//www.youtube.com/embed/NEwII4TCagM?rel=0" frameborder="0" allowfullscreen></iframe>
 <style> body, a, a:hover { cursor:url('http://s3.postimg.org/olhgjevdb/icon.png'), auto }</style>
 <body background="arkaplan.jpg"></body>
